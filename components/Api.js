4e6fc19a736a7ea07abb57679673aefbd2182b8b
Jimancio <54536722+ignacioHourquescos@users.noreply.github.com>
2021-11-14 00:07:37 -0300

    first commit

---
+++ components/Api.js
@@ -0,0 +1,51 @@
+import {useState, useEffect} from 'react'
+import 'antd/dist/antd.css';
+import { Table } from 'antd';
+
+
+const Api = ()=>{
+
+   const [coinArray, setCoinArray] = useState([])
+   const [loading, setLoading] =useState(true);
+
+
+   useEffect(()=>{
+      fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=1000&page=1&price_change_percentage=1h%2C24h%2C7d&sparkline=false")
+      .then(response => response.json())
+      .then(data=>{setCoinArray(data);setLoading(false)})
+   })
+
+
+   const columns = [
+      {
+        title: 'Moneda',
+        dataIndex: 'name',
+      //   sorter: (a, b) => a.name.length - b.name.length,
+      //   sortDirections: ['descend'],
+      },
+      // {
+      //   title: 'Value',
+      //   dataIndex: 'value',
+      //   defaultSortOrder: 'descend',
+      //   sorter: (a, b) => a.value - b.value,
+      // },
+      {
+         title: 'Variacion ultima hora en %',
+         dataIndex: 'price_change_percentage_1h_in_currency',
+         defaultSortOrder: 'descend',
+         sorter: (a, b) => a.price_change_percentage_1h_in_currency - b.price_change_percentage_1h_in_currency,
+      },
+    ];
+
+
+   return(<>
+   {
+      loading?null:
+      <Table columns={columns} dataSource={coinArray}     pagination={false}  />
+   }
+   
+   </>)
+
+}
+
+export default Api
